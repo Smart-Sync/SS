d71f8cd6fa235965fc0b490a1ae3699b3a558518
Gungun28 <kunalsharmaji440@gmail.com>
2024-09-12 20:27:07 +0530

    update: naming changes

--- src/admin_pages/ExpertPage.js
+++ src/admin_pages/ExpertPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar } from '../admin_components/Navbar'
-import { Experts } from '../admin_components/Experts'
+import { Experts } from '../admin_components/MappingResult'
 export const ExpertPage = () => {
   return (
     <div><Navbar><Experts></Experts></Navbar></div>
